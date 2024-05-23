3a6ea4ee3598ed0f23cc42f25b9987e734df9322
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-05-22 19:26:17 -0700

    LIFO caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -2,7 +2,8 @@
 """
     Basic dictionary caching
 """
-BaseCaching = __import__('base_caching').BaseCaching
+from collections import OrderedDict
+from base_caching import BaseCaching
 
 
 class LIFOCache(BaseCaching):
@@ -12,10 +13,19 @@ class LIFOCache(BaseCaching):
     def __init__(self):
         """The init method"""
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """insert into cache"""
-        pass
+        if key is None or item is None:
+            return
+        if len(self.cache_data) +1 > self.MAX_ITEMS:
+            poped, _ = self.cache_data.popitem()
+            print(f"DISCARD: {poped}")
+        self.cache_data[key] = item
 
     def get(self, key):
         """Retrieve the cached data"""
+        if key is None:
+            return None
+        return self.cache_data.get(key)
