6a0fb57084708306b10b9465c9c1f91d9fc32baf
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-05-22 18:48:14 -0700

    FIFO caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -15,7 +15,15 @@ class FIFOCache(BaseCaching):
 
     def put(self, key, item):
         """insert into cache"""
-        pass
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > self.MAX_ITEMS:
+            dis = self.cache_data.pop(list(self.cache_data.keys())[0])
+            print(f"DISCARD: {dis}")
 
     def get(self, key):
         """Retrieve the cached data"""
+        if key is None:
+            return None
+        return self.cache_data.get(key)
