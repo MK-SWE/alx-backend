51779f82f8332a7a9895a3e55e69c6088e756903
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-05-22 18:08:23 -0700

    basic caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -15,7 +15,12 @@ class BasicCache(BaseCaching):
 
     def put(self, key, item):
         """insert into cache"""
-        pass
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
 
     def get(self, key):
         """Retrieve the cached data"""
+        if key is None:
+            return None
+        return self.cache_data.get(key)
