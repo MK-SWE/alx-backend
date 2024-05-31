1a689cb54cd8b70d6a9816ba83a6082a6af32151
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-05-31 18:37:07 +0000

    change files permissions

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -14,10 +14,9 @@ class Config(object):
     """
         config the babel object
     """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -32,11 +31,11 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', strict_slashes=False)
-def index() -> str:
+@app.route('/')
+def index():
     """Render Basic Home page"""
     return render_template('2-index.html')
 
 
-if __name__ == "__main__":
-    app.run(port="5000", host="0.0.0.0", debug=True)
+if __name__ == '__main__':
+    app.run()
