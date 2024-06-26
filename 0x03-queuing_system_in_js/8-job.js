a3c16a4a1e5db01658bd35e4d433acd10ed2d1e5
MohammedMagdy2023 <magdy_mohammed2000551@outlook.com>
2024-06-26 05:41:15 +0300

    Writing the job creation function with test

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,19 @@
+function createPushNotificationsJobs(jobs, queue) {
+  if (!Array.isArray(jobs)) {
+    throw new Error('Jobs is not an array');
+  }
+  jobs.forEach((jobData) => {
+    const job = queue.create('push_notification_code', jobData);
+    job.save((error) => {
+      if (error) {
+        console.error(`Failed to create job: ${error}`);
+      } else {
+        console.log(`Notification job created: ${job.id}`);
+        job.on('complete', () => console.log(`Notification job ${job.id} completed`))
+          .on('failed', error => console.log(`Notification job ${job.id} failed: ${error}`))
+          .on('progress', (progress, data) => console.log(`Notification job ${job.id} ${progress}% complete`));
+      }
+    });
+  });
+}
+export default createPushNotificationsJobs;
---
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -0,0 +1,43 @@
+import { expect } from 'chai';
+import createPushNotificationsJobs from './8-job';
+import kue from 'kue';
+
+describe('createPushNotificationsJobs', () => {
+  let queue;
+
+  before(() => {
+    queue = kue.createQueue();
+    queue.testMode.enter();
+  });
+
+  afterEach(() => {
+    queue.testMode.clear();
+  });
+
+  after(() => {
+    queue.testMode.exit();
+  });
+
+  it('display a error message if jobs is not an array', () => {
+    expect(() => createPushNotificationsJobs('notAnArray', queue)).to.throw('Jobs is not an array');
+  });
+
+  it('create two new jobs to the queue', () => {
+    const jobs = [
+      {
+        phoneNumber: '4153518780',
+        message: 'This is the code 1234 to verify your account'
+      },
+      {
+        phoneNumber: '4153518781',
+        message: 'This is your verification code: 4321'
+      },
+    ];
+    createPushNotificationsJobs(jobs, queue);
+    expect(queue.testMode.jobs.length).to.equal(2);
+    expect(queue.testMode.jobs[0].type).to.equal('push_notification_code');
+    expect(queue.testMode.jobs[0].data).to.deep.equal(jobs[0]);
+    expect(queue.testMode.jobs[1].type).to.equal('push_notification_code');
+    expect(queue.testMode.jobs[1].data).to.deep.equal(jobs[1]);
+  });
+});
